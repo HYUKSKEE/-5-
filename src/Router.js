604c7e3c360d0276aecc238c9914a2da9644f9ee
HYUKSKEE <gin280833@gmail.com>
2022-08-03 20:16:53 +0900

    초기세팅 및 폴더 세팅 완료함.

---
+++ src/Router.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import LoginJHKim from './pages/JHkim/Login/Login';
+import MainJHKim from './pages/JHkim/Main/Main';
+
+import LoginJHSo from './pages/JHso/Login/Login';
+import MainJHSo from './pages/JHso/Main/Main';
+
+import LoginJSHam from './pages/JSham/Login/Login';
+import MainJSHam from './pages/JSham/Main/Main';
+
+import LoginSYHa from './pages/SYha/Login/Login';
+import MainSYHa from './pages/SYha/Main/Main';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/logink" element={<LoginJHKim />} />
+        <Route path="/maink" element={<MainJHKim />} />
+        <Route path="/logins" element={<LoginJHSo />} />
+        <Route path="/mains" element={<MainJHSo />} />
+        <Route path="/loginh" element={<LoginJSHam />} />
+        <Route path="/mainh" element={<MainJSHam />} />
+        <Route path="/loginha" element={<LoginSYHa />} />
+        <Route path="/mainha" element={<MainSYHa />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
